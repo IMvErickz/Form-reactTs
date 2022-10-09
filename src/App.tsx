a98f4e3b37fa0f83a08406180d6d4b00961c33d7
IMvErickz <erickspy2003@gmail.com>
2022-10-09 19:05:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function NavBar() {
+  
+
+  return (
+    <header>
+       <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Formulário</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+      </ul>
+    </div>
+  </nav>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <body>
+      <div id='principal'>
+    <fieldset id='camp'>
+       <div className="row">
+        <div className="input-field col s6">
+          <input id="first_name2" type="text" className="validate" />
+          <label className="active" htmlFor="first_name2">Nome</label>
+        </div>
+      </div>
+      
+      <div className="row">
+        <div className="input-field col s6">
+          <input id="first_name2" type="text" className="validate" />
+          <label className="active" htmlFor="first_name2">Sobrenome</label>
+        </div>
+          </div>
+    </fieldset>
+          <div>
+        <a className="waves-effect waves-light btn">Enviar</a>
+        </div>
+      </div>  
+    </body>
+  )
+}
+
+export default {App, NavBar}
