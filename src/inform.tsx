743df02165ff2853a4352e8162de4f1db0c47f1a
IMvErickz <erickspy2003@gmail.com>
2022-10-09 21:14:39 -0300

    added recuparete LocalStorage and style

--- src/inform.tsx
+++ src/inform.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import './assets/inform.css'
+import Logo from './assets/cadastre-se.png'
+
 
 function App() {
     return (
         <header>
             <nav>
               <div className="nav-wrapper">
-                <a href="#" className="brand-logo center">Logo</a>
+                <a href="#" className="brand-logo center"><img id='logo' src={Logo} alt="" /></a>
                 <ul id="nav-mobile" className="left hide-on-med-and-down">
                   <li><a href="/index.html">Formulário</a></li>
                 </ul>
@@ -16,12 +19,34 @@ function App() {
 }
 
 function bod() {
+
+    function show(){
+        let rec = JSON.parse(localStorage.getItem("Pessoa"))
+        let spanName = document.getElementById('name')
+        let spanLast = document.getElementById('lastName')
+        spanName.innerHTML = rec.Nome
+        spanLast.innerHTML = rec.Sobrenome
+
+
+    }
+
+    
+
     return (
         <body>
+        <a className="waves-effect waves-light btn" onClick={show}>Mostrar informações</a>
+
             <fieldset>
                 <div>
-                    <span id='name'></span>
-                    <span id='lastName'></span>
+                    <ul>
+                        <li>
+                            <span id='name'></span>
+                        </li>
+                        <li>
+                            <span id='lastName'></span>
+
+                        </li>
+                    </ul>
                 </div>
             </fieldset>
         </body>
